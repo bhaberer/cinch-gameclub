c3b0401a9653e292758dcf63ee2f727e6c596def
Brian Haberer <bhaberer@gmail.com>
2014-05-19 15:26:41 -0700

    Updating regexp so we only need one.

--- lib/cinch/plugins/gameclub.rb
+++ lib/cinch/plugins/gameclub.rb
@@ -31,33 +31,11 @@ module Cinch::Plugins
                      }
       @gameclub_free_games = [ 440, 630, 218230 ]
       @gameclub_games = @steam_games.keys - @gameclub_free_games
-      WebApi.api_key = config[:steam_apikey] 
+      # = config[:steam_apikey]
 
     end
-
-    class User < Struct.new(:nick, :steamid, :aliases, :games)
-      def to_yaml
-        { :nick => nick, :steamid => steamid, :aliases => aliases, :games => games }
-      end
-
-      def refresh_games
-        id = SteamId.from_steam_id(steamid)
-        steam_games = id.games
-        games = steam_games & @gameclub_games
-      end
-
-      def save
-        @storage.data[:users][nick] = User.new(nick, steamid, aliases, games)
-        @storage.synced_save(@bot)
-      end
-
-      def self.get(nick)
-        @storage.data[:users][nick]
-      end
-    end
-
-    match /gameclub ([^\s]+)\z/
-    match /gameclub ([^\s]+) (.+)/
+    
+    match /gameclub ([^\s]+)(?:\s(.+))?/
 
     def execute(m, command, args = nil)
       case command
